a12078779b78631e2298db5de4487c5ee2d2e8d6
anand nandhu <anandnandhu52@gmail.com>
2023-03-14 11:38:22 +0530

    jenkinscode

--- vars/test1.groovy
+++ vars/test1.groovy
@@ -9,4 +9,19 @@ def new1() {
     } else {
         print "no"
     }
+
+    def x=2
+    def y=0
+    while(x > y) {
+        println"${x}"
+        y++
+    }
+
+    for(int i = 0;i<5;i++) {
+        println(i);
+    }
+    def array = [0,1,2,3];
+    for(i in array) {
+        println(i);
+    }
 }
