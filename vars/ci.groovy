8204e439117e87464441c5285fb0a028d44722c2
anand nandhu <anandnandhu52@gmail.com>
2023-03-31 20:33:27 +0530

    jenkinscode

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -4,6 +4,12 @@ def call() {
         env.SONAR_EXTRA_OPTS = " "
     }
 
+    if(!env.TAG_NAME) {
+        env.PUSH_CODE = "false"
+    } else {
+        env.PUSH_CODE = "true"
+    }
+
     try {
         node('workstation') {
 
@@ -26,9 +32,10 @@ def call() {
                     sh "echo sonar scan"
                 }
             }
-
-            stage('Upload Code to Centralized Place') {
-                echo 'Upload'
+            if(env.PUSH_CODE == "true") {
+                stage('Upload Code to Centralized Place') {
+                    echo 'Upload'
+                }
             }
 
         }
