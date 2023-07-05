ed8e28a64f999377130d502b44f28642a21b429a
anand nandhu <anandnandhu52@gmail.com>
2023-07-05 15:31:07 +0530

    docker

--- vars/common.groovy
+++ vars/common.groovy
@@ -5,6 +5,7 @@ def compile() {
     if (app_lang == "maven") {
         sh "mvn package && cp target/${component}-1.0.jar ${component}.jar"
     }
+    sh "docker build -t ${component} . "
 }
 
 def unittests() {
